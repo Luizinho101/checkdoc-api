65acb8959f15dbb1dc7ab6aeb1ebf231d431ba24
Luizinho101 <luiz.antonio.da.silva777@gmail.com>
2026-06-06 23:06:06 -0300

    feat: estrutura inicial do projeto

---
+++ src/app.js
@@ -0,0 +1,15 @@
+
+const express = require('express');
+
+const app = express();
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    return res.status(200).json({ 
+        message: 'CheckDoc API online e operacional!' 
+    });
+});
+
+
+module.exports = app;
